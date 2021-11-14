cd5e06f366e53d204a5cdff6284efeaf8b12afc2
Aku Sarma <akusarma1702@gmail.com>
2021-11-14 18:59:38 +0530

    Created a back button

--- main.py
+++ main.py
@@ -17,42 +17,53 @@ class Main():
         self.classXIFrame = Frame(root)
         self.classXIIFrame = Frame(root)
         self.chaptersFrame = Frame(root)
+        self.backButton = Button(root, text="back", command=self.back)
+        self.currentScreen = ""
 
     def mainMenu(self):
+        self.currentScreen = "mainMenu"
         
-        labelMain = Label(self.mainFrame, text="Online courses", width=20).pack(pady=10)
-        classXI = Button(self.mainFrame, text="class XI", width=20, command=self.classXI).pack(pady=10)
-        classXII = Button(self.mainFrame, text="class XII", width=20, command=self.classXII).pack(pady=10)
+        labelMain = Label(self.mainFrame, text="Online courses", width=20).grid(row=0, column=0, columnspan=3, pady=40)
+        classXI = Button(self.mainFrame, text="class XI", width=20, command=self.classXI).grid(row=1, column=0, padx=20)
+        classXII = Button(self.mainFrame, text="class XII", width=20, command=self.classXII).grid(row=1, column=1, padx=20)
 
         self.mainFrame.pack(pady=200)
     
     def classXI(self):
-        self.mainFrame.destroy()
-        self.mainFrame.pack_forget()
+        if self.currentScreen == "mainMenu":
+            self.mainFrame.pack_forget()
 
-        chemistry = Button(self.classXIFrame, text="chemistry", width=20, command=lambda: self.chapters('classXI', 'chemistry')).pack(pady=10)
-        physics = Button(self.classXIFrame, text="Physics", width=20, command=lambda: self.chapters('classXI', 'physics')).pack(pady=10)
-        maths = Button(self.classXIFrame, text="Maths", width=20, command=lambda: self.chapters('classXI', 'maths')).pack(pady=10)
+            self.backButton.pack(anchor=NW)
+
+            chemistry = Button(self.classXIFrame, text="chemistry", width=20, command=lambda: self.chapters('classXI', 'chemistry')).pack(pady=10)
+            physics = Button(self.classXIFrame, text="Physics", width=20, command=lambda: self.chapters('classXI', 'physics')).pack(pady=10)
+            maths = Button(self.classXIFrame, text="Maths", width=20, command=lambda: self.chapters('classXI', 'maths')).pack(pady=10)
+
+        self.currentScreen = "classXI"
 
         self.classXIFrame.pack(pady=200)
 
     def classXII(self):
-        self.mainFrame.destroy()
-        self.mainFrame.pack_forget()
+        if self.currentScreen == "mainMenu":
+            self.mainFrame.pack_forget()
 
-        chemistry = Button(self.classXIIFrame, text="chemistry", width=20, command=lambda: self.chapters('classXII', 'chemistry')).pack(pady=10)
-        physics = Button(self.classXIIFrame, text="Physics", width=20, command=lambda: self.chapters('classXII', 'physics')).pack(pady=10)
-        maths = Button(self.classXIIFrame, text="Maths", width=20, command=lambda: self.chapters('classXII', 'maths')).pack(pady=10)
+            self.backButton.pack(anchor=NW)
+
+            chemistry = Button(self.classXIIFrame, text="chemistry", width=20, command=lambda: self.chapters('classXII', 'chemistry')).pack(pady=10)
+            physics = Button(self.classXIIFrame, text="Physics", width=20, command=lambda: self.chapters('classXII', 'physics')).pack(pady=10)
+            maths = Button(self.classXIIFrame, text="Maths", width=20, command=lambda: self.chapters('classXII', 'maths')).pack(pady=10)
+
+        self.currentScreen = "classXII"
 
         self.classXIIFrame.pack(pady=200)
 
     def chapters(self, classno, subject):
         if classno=="classXII":
-            self.classXIIFrame.destroy()
             self.classXIIFrame.pack_forget()
+            self.currentScreen = "chaptersClassXII"
         else:
-            self.classXIFrame.destroy()
             self.classXIFrame.pack_forget()
+            self.currentScreen = "chaptersClassXI"
         global data
         
         subjects = []
@@ -64,12 +75,33 @@ class Main():
         for i in subjects:
             links.append(data[classno][subject][i])
 
+        self.backButton.pack(anchor=NW)
+
         for index, subject in enumerate(subjects):
             subject = Button(self.chaptersFrame, text=subject, width=50, command=lambda: webbrowser.open(links[index]))
             
             subject.pack(pady=3)
 
         self.chaptersFrame.pack(pady=50)
+
+    def back(self):
+        if self.currentScreen == "classXII":
+            self.classXIIFrame.pack_forget()
+            self.backButton.pack_forget()
+            self.mainMenu()
+
+        elif self.currentScreen == "classXI":
+            self.classXIFrame.pack_forget()
+            self.backButton.pack_forget()
+            self.mainMenu()
+
+        elif self.currentScreen == "chaptersClassXII":
+            self.chaptersFrame.pack_forget()
+            self.classXII()
+
+        else:
+            self.chaptersFrame.pack_forget()
+            self.classXI()
 
 
 if __name__ == '__main__':
